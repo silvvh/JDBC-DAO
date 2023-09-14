382ce6d4e2f1b2ff1271fabb732d32fa2bb33635
Victor Brito <victorcyty@hotmail.com>
2023-09-13 22:47:50 -0300

    Classe vendedor implementada

---
+++ src/Model/Entities/Seller.java
@@ -0,0 +1,90 @@
+package Model.Entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(getId(), seller.getId()) && Objects.equals(getName(), seller.getName()) && Objects.equals(getEmail(), seller.getEmail()) && Objects.equals(getBirthDate(), seller.getBirthDate()) && Objects.equals(getBaseSalary(), seller.getBaseSalary());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getEmail(), getBirthDate(), getBaseSalary());
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Seller{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", birthDate=").append(birthDate);
+        sb.append(", baseSalary=").append(baseSalary);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Seller() {}
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+    }
+}
