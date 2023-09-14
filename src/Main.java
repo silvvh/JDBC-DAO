d566b81bf66cb916c498f6a8adfb460dd5c3d86a
Victor Brito <victorcyty@hotmail.com>
2023-09-13 22:32:47 -0300

    Classe departamento implementada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import Model.Entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-
+        Department d1 = new Department(1, "Books");
+        System.out.println(d1);
     }
 }
---
+++ src/Model/Entities/Department.java
@@ -0,0 +1,56 @@
+package Model.Entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    public Department () {}
+
+    public Department (Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        String sb = "Department{" + "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+        return sb;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return getId() != null ? getId().equals(that.getId()) : that.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+}
